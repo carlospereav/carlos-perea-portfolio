3df3fd919238e9978726488d6c93aeb924b89453
carlospereav <carlospereav8@gmail.com>
2025-12-08 22:57:55 +0100

    fix🔥: configure webpack alias for both client and server

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     };
     return config;
   },
